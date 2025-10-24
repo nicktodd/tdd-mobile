a78381f102569baf7d592718107248a27100fe08
Nick Todd <nick.todd@conygre.com>
2025-10-24 16:21:51 +0100

    Created a Starwars Swift project with tests

---
+++ ios/demos/starwars-api/StarwarsAPI/StarwarsAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarwarsAPI
+//
+//  Created by Nick Todd on 24/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/demos/starwars-api/StarwarsAPI/StarwarsAPI/StarwarsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarwarsAPIApp.swift
+//  StarwarsAPI
+//
+//  Created by Nick Todd on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StarwarsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
